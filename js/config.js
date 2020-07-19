3e2acc5585ef28794e8e7bcd003f77a2c6336eff
awspractice2099 <68506540+awspractice2099@users.noreply.github.com>
2020-07-20 00:38:16 +0530

    Test Pages
    Test web pages for website

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_IJIaRrfeU',
+        userPoolClientId: '527k56nr2uemd37fbkpij8d1jtn',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
+    }
+};
